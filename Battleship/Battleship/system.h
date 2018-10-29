af835c777e4e10a8000ab90235b9e1ca6821bc31
Jeffrey Iacob <36392018+JeffreyI@users.noreply.github.com>
2018-10-29 15:31:54 -0700

    Update system.h

--- Battleship/Battleship/system.h
+++ Battleship/Battleship/system.h
@@ -1,10 +1,14 @@
-#pragma once
 #ifndef B_System_h
+#define B_System_h
 #include"user.h"
+#include"gameMaster.h"
+#include"adminTools.h"
+#include"authenticate.h"
+
+#include<vector>
 #include<iostream>
 #include<fstream>
 using namespace std;
-
 
 class B_System
 {
@@ -17,6 +21,7 @@ public:
 	void login();
 private:
 	user user;
+	authenticate auth;
 	ifstream fin;
 	ofstream fout;
 };
@@ -31,15 +36,15 @@ B_System::~B_System()
 
 void B_System::welcome()
 {
+	user.createObj(fin);
 	cout << "Hello and welcome to Battleship!\n";
-	cout << "Please create an accout or login.\n";
+	cout << "Please select an option\n";
 }
 
 void B_System::goodbye()
 {
 	cout << "Thanks for playing Battleship, ";
-	user.printName();
-	exit(0);
+	cout << user.getName();
 }
 
 void B_System::signup()
@@ -56,13 +61,14 @@ void B_System::signup()
 	cin >> pass;
 	user.password(pass);
 
+	user.ID("player");
+
 	//adding user to file
 
 	fin.open("user.txt");
 	string word;
 	if (fin.is_open())
 	{
-
 		while (getline(fin, word, ' '))
 		{
 			if (word == name)
@@ -82,7 +88,7 @@ void B_System::signup()
 	fout.open("user.txt", std::ofstream::out | std::ofstream::app);
 	if (fout.is_open())
 	{
-		fout << name << " " << pass << endl;
+		fout << user.getName() << " " << user.getPassword() << " " << user.getID() << endl;
 		fout.close();
 	}
 	else
@@ -93,9 +99,23 @@ void B_System::signup()
 
 void B_System::login()
 {
-	if (user.auth.validate(user.getName(), user.getPassword(), fin))
+	string username, password;
+	cout << "Username: ";
+	cin >> username;
+	cout << "Password: ";
+	cin >> password;
+
+	if (auth.validate(username, password, user.obj))
 	{
 		cout << "Login in Successfuly!\n";
+		if (user.getID() == "player")
+		{
+			gameMaster().showOptions();
+		}
+		else
+		{
+			adminTools().showTools();
+		}
 	}
 	else
 	{
