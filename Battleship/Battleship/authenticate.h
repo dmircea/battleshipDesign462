d2f7d889322dcfe86df14065ef70c63f77c25fac
dmircea <dmircea4you@gmail.com>
2018-10-29 12:37:01 -0700

    Made a lot of changes to Authenticate,user,system
    Added the Abstract UI Layers and changed authenticate to work with that.

--- Battleship/Battleship/authenticate.h
+++ Battleship/Battleship/authenticate.h
@@ -1,1 +1,62 @@
 #pragma once
+#include <string>
+#include <fstream>
+#include "UserUIABS.h"
+
+class authenticate : public UserUI_ABS
+{
+public:
+    authenticate()
+    {
+
+    }
+    authenticate(std::string, std::string);
+    ~authenticate()
+    {
+
+    }
+
+    bool validate(std::string, std::string);
+    void signup();
+    void login();
+
+    void showOptions() override;
+    char getChoice() override;
+
+
+private:
+    std::string username;
+    std::string password;
+    std::ifstream fin;
+    std::ofstream fout;
+};
+
+bool authenticate::validate(std::string username, std::string pass)
+{
+    //fin.open("user.txt");
+
+    std::cout << "User has been validated.";
+    return true;
+}
+
+
+//
+void authenticate::showOptions()
+{
+
+}
+
+char authenticate::getChoice()
+{
+
+
+    return 'a';
+}
+
+
+
+authenticate::authenticate(std::string crtUserName, std::string crtpass)
+{
+    username = crtUserName;
+    password = crtpass;
+}
--- Battleship/Battleship/main.cpp
+++ Battleship/Battleship/main.cpp
@@ -6,7 +6,7 @@ int main()
 {
 	B_System B_System;
 	B_System.welcome();
-	B_System.signup();
+	//B_System.signup();
 	B_System.goodbye();
 
 	system("PAUSE");
