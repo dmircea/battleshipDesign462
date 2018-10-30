9d9ba755c9ba0961a6a934fa67ef32c9d6c94c78
Jeffrey Iacob <36392018+JeffreyI@users.noreply.github.com>
2018-10-30 16:15:10 -0700

    Update adminTools.h

--- Battleship/Battleship/adminTools.h
+++ Battleship/Battleship/adminTools.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "UserUIABS.h"
+#include <thread>
+#include <ctime>
 
 class adminTools : public AdminUI_ABS
 {
@@ -11,7 +13,7 @@ public:
 
     void seeServers() override;
     void findPlayerByID() override;
-
+	UserUI_ABS * serverMainteneance() override;
 private:
 };
 
@@ -26,22 +28,68 @@ adminTools::~adminTools()
 
 int adminTools::showOptions()
 {
+	int choice = 0;
+	std::cout << "1. Server Maintenenace\n"
+		<< "2. Veiw servers\n"
+		<< "3. Find Player\n"
+		<< "4. Log out\n"
+		<< "5. Exit\n";
 
+	std::cin >> choice;
 
-    return 1;
+	return choice;
 }
 UserUI_ABS* adminTools::getChoice(int choice)
 {
-
-
+	switch (choice)
+	{
+	case 1:
+		return serverMainteneance();
+		break;
+	case 2:
+		seeServers();
+		break;
+	case 3:
+		findPlayerByID();
+		break;
+	case 4:
+		break;
+	case 5:
+		exit(0);
+		break;
+	default:
+		std::cout << "Incorrect input, please try again!";
+		break;
+	}
     return this;
 }
 
 void adminTools::seeServers()
 {
-
+	std::cout << "Battleship Servers:"
+		<< "Adress \t Reason \t Created\n"
+		<< "\t\t91.121.44.240 \t Battleship \t Sep 15, 2018\n"
+		<< "\t\t109.248.203.195  Battleship \t Oct 10, 2018\n"
+		<< "\t\t90.139.36.50 \t Battleship \t Oct 28, 2018\n";
 }
 void adminTools::findPlayerByID()
 {
 
+}
+
+UserUI_ABS * adminTools::serverMainteneance()
+{
+	std::string lenght;
+	std::string date;
+	std::string time;
+	bool maintenance = false;
+
+	std::cout << "Enter the length, date and time: ";
+	std::cin >> lenght >> date >> time;
+
+	std::cout << "Performing server maintenance: "
+		<< "on " << date << " at " << time << " for " << lenght  << "min" << std::endl;
+	maintenance = true;
+
+	return this;
 }
