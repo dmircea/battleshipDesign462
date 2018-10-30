fe2e6b93db33a0569ef741b98aadaed4e6490a81
Jeffrey Iacob <36392018+JeffreyI@users.noreply.github.com>
2018-10-29 17:23:13 -0700

    Update adminTools.h

--- Battleship/Battleship/adminTools.h
+++ Battleship/Battleship/adminTools.h
@@ -1,1 +1,23 @@
 #pragma once
+class adminTools
+{
+public:
+	adminTools();
+	~adminTools();
+	void showTools();
+private:
+
+};
+
+adminTools::adminTools()
+{
+}
+
+adminTools::~adminTools()
+{
+}
+
+void adminTools::showTools()
+{
+
+}
