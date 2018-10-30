79bdf0824187c7c27409f1e97db135ff4f112cf1
Jeffrey Iacob <36392018+JeffreyI@users.noreply.github.com>
2018-10-29 17:22:30 -0700

    Update main.cpp

--- Battleship/Battleship/main.cpp
+++ Battleship/Battleship/main.cpp
@@ -6,8 +6,30 @@ int main()
 {
 	B_System B_System;
 	B_System.welcome();
-	B_System.signup();
-	B_System.goodbye();
 
+	cout << "1. Signup\n"
+		<< "2. Login\n"
+		<< "3. Exit\n";
+	
+	bool done = false;
+	int opt;
+	
+	while (!done)
+	{
+		cin >> opt;
+		switch (opt)
+		{
+		case 1:
+			B_System.signup();
+			break;
+		case 2:
+			B_System.login();
+			break;
+		case 3:
+			B_System.goodbye();
+			done = true;
+			break;
+		}
+	}
 	system("PAUSE");
 }
