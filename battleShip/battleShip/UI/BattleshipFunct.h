c27cd2391b6c2308e12dd9b6275d76a5e38a02ed
Jeffrey Iacob <36392018+JeffreyI@users.noreply.github.com>
2018-12-05 14:23:50 -0800

    Battleship + Ships

--- battleShip/battleShip/UI/BattleshipFunct.h
+++ battleShip/battleShip/UI/BattleshipFunct.h
@@ -12,12 +12,13 @@ Board * Battleship::createBoard()
 		submarine = false, 
 		cruiser = false, 
 		destroyer = false;
+	bool placed = false;
 	int choice;
 
 	while (currentBoard->getShipsPlaced() < 5)
 	{
 		std::cout << "This is the board, where do you want to place the ship?" << std::endl;
-		std::cout << "Ships to be places: 1. Aircraftcarrier\n 2. Battleship\n 3. Submarine\n 4. Cruiser\n 5. Destroyer" << std::endl;
+		std::cout << "Ships to be places:\n 1. Aircraftcarrier\n 2. Battleship\n 3. Submarine\n 4. Cruiser\n 5. Destroyer" << std::endl;
 
 		std::cin >> choice;
 		switch (choice)
@@ -26,50 +27,65 @@ Board * Battleship::createBoard()
 			if (aircraft == true)
 			{
 				std::cout << "Ship already placed!\n" << std::endl;
+				placed = true;
+				break;
 			}
 			else
 			{
 				aircraft = true;
+				placed = false;
 				break;
 			}
 		case 2:
 			if (battle == true)
 			{
 				std::cout << "Ship already placed!\n" << std::endl;
+				placed = true;
+				break;
 			}
 			else
 			{
 				battle = true;
+				placed = false;
 				break;
 			}
 		case 3:
 			if (submarine == true)
 			{
 				std::cout << "Ship already placed!\n" << std::endl;
+				placed = true;
+				break;
 			}
 			else
 			{
 				submarine = true;
+				placed = false;
 				break;
 			}
 		case 4:
 			if(cruiser == true)
 			{
 				std::cout << "Ship already placed!\n" << std::endl;
+				placed = true;
+				break;
 			}
 			else
 			{
 				cruiser = true;
+				placed = false;
 				break;
 			}
 		case 5:
 			if(destroyer == true)
 			{
 				std::cout << "Ship already placed!\n" << std::endl;
+				placed = true;
+				break;
 			}
 			else
 			{
 				destroyer = true;
+				placed = false;
 				break;
 			}
 		default:
@@ -77,15 +93,18 @@ Board * Battleship::createBoard()
 			break;
 		}
 
-		currentBoard->printBoard();
-		std::cout << "What x value? ";
-		std::cin >> x;
-		std::cout << "What y value? ";
-		std::cin >> y;
-
-		if (currentBoard->placeShip(x, y, choice))
+		if (!placed)
 		{
-			currentBoard->incrementShipsPlaced();
+			currentBoard->printBoard();
+			std::cout << "What x value? ";
+			std::cin >> x;
+			std::cout << "What y value? ";
+			std::cin >> y;
+
+			if (currentBoard->placeShip(x, y, choice))
+			{
+				currentBoard->incrementShipsPlaced();
+			}
 		}
 	}
     return currentBoard;
